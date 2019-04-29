2a33cfc7551f87c6fdffe6be8054aea0a1883a4d
Dayuan Tang <jimmytangda@outlook.com>
2019-04-29 15:02:48 -0700

    lightbox done, start and end bug fixed, display centered

--- assets/script/interest.js
+++ assets/script/interest.js
@@ -1,1 +1,30 @@
-$(document).ready
+$(document).ready(function() {
+    const distanceToNextImage = -450;
+    var currentImageNumber = 0;
+    
+    $("#lightbox").hide()
+
+    $("#right").click(function() {
+        if (currentImageNumber < 7) {
+            currentImageNumber++;
+        }
+        $("#carousel-strip").css("left", (distanceToNextImage*currentImageNumber).toString() + "px")
+    })
+
+    $("#left").click(function() {
+        if (currentImageNumber > 0) {
+            currentImageNumber--;
+        }
+        $("#carousel-strip").css("left", (distanceToNextImage*currentImageNumber).toString() + "px")
+    })
+
+    $("#close, #overlay").click(function() {
+        $("#lightbox").hide()
+    })
+
+    $(".image").click(function() {
+        currentImageNumber = parseInt(this.id)
+        $("#carousel-strip").css("left", (distanceToNextImage*currentImageNumber).toString() + "px")
+        $("#lightbox").show()
+    })
+})
